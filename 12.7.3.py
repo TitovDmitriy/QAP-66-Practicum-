9ce1039d6a069ebe03d11b6e5bd9b62fa6e48d5a
TitovDmitriy <94840048+TitovDmitriy@users.noreply.github.com>
2021-12-29 22:56:58 +0300

    Update 12.7.3.py

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,4 +1,4 @@
-money = int(input('Введите сумму вклада'))
+money = float(input('Введите сумму вклада'))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = []
 for key in per_cent:
