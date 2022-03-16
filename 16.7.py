73d657049f8eff67841401f7ab16c623f872c83f
gdc5kyznfric <gdc5kyznfric@mail.ru>
2022-03-16 22:10:02 +0300

    16.7

---
+++ 16.7.py
@@ -0,0 +1,12 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        
+def GetName(self):
+    return self.name
+def GetGender(self):
+    return self.gender
+def GetAge(self):
+    return self.age
